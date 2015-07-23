e4a12917d1d70dacf8b88fbfcfc51575f6048fe0
Joe Leinsdorf <leinsdorf@gmail.com>
2015-07-23 15:43:50 -0400

    dev projects displaying

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,21 @@
 $(function(){
 
-  skrollr.init({});
+  skrollr.init();
+
+
+  var waypoint = new Waypoint({
+    element: document.getElementById('slide-3'),
+    handler: function() {
+      typeWebDev();
+    }
+  });
+
 
 });
+
+var typeWebDev = function() {
+  $('#jac-heading').typed({
+      strings: ['JAC Soundfactory'],
+      typeSpeed: 30
+  });
+};
